6a8ff474bcf51beca942875b712d34e308d4eceb
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2024-09-19 12:22:17 -0700

    ablation figures code

--- scripts/get_mace_labels.py
+++ scripts/get_mace_labels.py
@@ -25,14 +25,17 @@ def record_and_save(dataset, file_path, fn):
     print(f"All tensors saved to LMDB:{file_path}")
 
 def record_labels(labels_folder, dataset_path, model="large"):
-    os.makedirs(labels_folder, exist_ok=True)
+    # os.makedirs(labels_folder, exist_ok=True)
     # Load the dataset
     train_dataset = registry.get_dataset_class("lmdb")({"src": os.path.join(dataset_path, 'train')})
     
     val_dataset = registry.get_dataset_class("lmdb")({"src": os.path.join(dataset_path, 'val')})
     
     # Load the model
-    calc = mace_mp(model=model, dispersion=False, default_dtype="float32", device='cuda')
+    if "mace_mp" in labels_folder:
+        calc = mace_mp(model=model, dispersion=False, default_dtype="float32", device='cuda')
+    elif "mace_off" in labels_folder:
+        calc = mace_off(model=model, dispersion=False, default_dtype="float32", device='cuda')
 
     def get_forces(sample):
         atomic_numbers = sample.atomic_numbers.numpy()
@@ -61,27 +64,49 @@ def record_labels(labels_folder, dataset_path, model="large"):
     
     atom_embeddings = get_atom_embeddings()
     np.save(os.path.join(labels_folder, 'atom_embeddings.npy'), get_atom_embeddings())
-    # record_and_save(train_dataset, os.path.join(labels_folder, 'final_node_features.lmdb'), get_final_node_features)
-    # record_and_save(val_dataset, os.path.join(labels_folder, 'final_node_features.lmdb'), get_final_node_features)
+    record_and_save(train_dataset, os.path.join(labels_folder, 'train_final_node_features', 'final_node_features.lmdb'), get_final_node_features)
+    record_and_save(val_dataset, os.path.join(labels_folder, 'val_final_node_features', 'final_node_features.lmdb'), get_final_node_features)
     # record_and_save(train_dataset, os.path.join(labels_folder, 'train_forces.lmdb'), get_forces)
     # record_and_save(train_dataset, os.path.join(labels_folder, 'force_jacobians.lmdb'), get_hessians)
     # record_and_save(val_dataset, os.path.join(labels_folder, 'val_forces.lmdb'), get_forces)
 
 if __name__ == "__main__":
-    labels_folder = '/data/shared/ishan_stuff/labels/mace_off_large_SpiceMonomers'
-    dataset_path = '/data/ishan-amin/SPICE/spice_separated/DES370K_Monomers'
+    '''Mace-MP on MPTraj'''
+    # labels_folder = '/data/shared/ishan_stuff/labels_unlocked/mace_mp_all_splits_Bandgap_greater_5'
+    # dataset_path = '/data/shared/ishan_stuff/mptraj_seperated_all_splits_unlocked/Bandgap_greater_than_5'
+
+    # record_labels(labels_folder, dataset_path)
+
+    # labels_folder = '/data/shared/ishan_stuff/labels_unlocked/mace_mp_all_splits_Yttrium'
+    # dataset_path = '/data/shared/ishan_stuff/mptraj_seperated_all_splits_unlocked/Yttrium'
+
+    # record_labels(labels_folder, dataset_path)
+
+
+    # labels_folder = '/data/shared/ishan_stuff/labels_unlocked/mace_mp_all_splits_Perovskites_noIons'
+    # dataset_path = '/data/shared/ishan_stuff/mptraj_seperated_all_splits_unlocked/Perovskites_noIons'
+
+
+    # record_labels(labels_folder, dataset_path)
+
+    '''Mace-OFF on SPICE'''
+    labels_folder = '/data/shared/ishan_stuff/labels_unlocked/mace_off_large_SpiceAminos'
+    dataset_path = '/data/shared/ishan_stuff/spice_separated/Solvated_Amino_Acids'
+    record_labels(labels_folder, dataset_path)
+
+    labels_folder = '/data/shared/ishan_stuff/labels_unlocked/mace_off_large_SpiceIodine'
+    dataset_path = '/data/shared/ishan_stuff/spice_separated/Iodine'
 
     record_labels(labels_folder, dataset_path)
 
-    labels_folder = '/data/shared/ishan_stuff/labels/mace_off_large_SpiceIodine'
-    dataset_path = '/data/ishan-amin/SPICE/spice_separated/Iodine'
+
+    labels_folder = '/data/shared/ishan_stuff/labels_unlocked/mace_off_large_SpiceMonomers'
+    dataset_path = '/data/shared/ishan_stuff/spice_separated/DES370K_Monomers'
+
 
     record_labels(labels_folder, dataset_path)
 
 
-    labels_folder = '/data/shared/ishan_stuff/labels/mace_off_large_SpiceAminos'
-    dataset_path = '/data/ishan-amin/SPICE/spice_separated/Solvated_Amino_Acids'
 
-    record_labels(labels_folder, dataset_path)
 
 
