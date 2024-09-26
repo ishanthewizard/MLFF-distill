c18c33b18e61af1dc5f04d0443143b60a42c7047
Sanjeev <sanjeevr@berkeley.edu>
2024-09-25 21:09:41 -0700

    hessian subsample ablations

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 
-python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/iodine/distill/gemnet-dT-small-teacherforces.yml --identifier iodine-gemSmall-DIST-teacherforces
-python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/monomers/distill/gemnet-dT-small-teacher.yml --identifier monomers-gemSmall-DIST-teacherforces
+# python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/iodine/distill/gemnet-dT-small-teacherforces.yml --identifier iodine-gemSmall-DIST-teacherforces
+# python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/monomers/distill/gemnet-dT-small-teacherforces.yml --identifier monomers-gemSmall-DIST-teacherforces
 
-python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/iodine/distill/painn-small-teacherforces.yml --identifier iodine-PaiNN-DIST-teacherforces
-python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/monomers/distill/painn-small-teacher.yml --identifier monomers-PaiNN-DIST-teacherforces
+# python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/solvated_amino_acids/distill/gemnet-dT-small.yml --optim.force_jac_sample_size=1 --identifier solvated-gemSmall-DIST-s1
+python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/solvated_amino_acids/distill/gemnet-dT-small.yml --optim.force_jac_sample_size=15 --identifier solvated-gemSmall-DIST-s15
+
+
+# python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/solvated_amino_acids/distill/painn-small.yml --optim.force_jac_sample_size=1 --identifier solvated-PaiNN-DIST-pbc-s1
+python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/solvated_amino_acids/distill/painn-small.yml --optim.force_jac_sample_size=10 --identifier solvated-PaiNN-DIST-pbc-s10
+python main.py --num-gpus 1 --submit --nersc --mode train --config-yml configs/SPICE/solvated_amino_acids/distill/painn-small.yml --optim.force_jac_sample_size=15 --identifier solvated-PaiNN-DIST-pbc-s15
+
