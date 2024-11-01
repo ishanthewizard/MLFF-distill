382935f055af3b731475b1fcb14232df33bb1fba
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2024-11-01 10:37:09 -0700

    change fairchem from submodule to folder

---
+++ fairchem/src/fairchem/data/oc/core/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from .adsorbate import Adsorbate
+from .adsorbate_slab_config import AdsorbateSlabConfig
+from .bulk import Bulk
+from .multi_adsorbate_slab_config import MultipleAdsorbateSlabConfig
+from .slab import Slab
---
+++ fairchem/src/fairchem/data/oc/databases/pkls/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+import os
+
+BULK_PKL_PATH = os.path.join(__path__[0], "bulks.pkl")
+ADSORBATE_PKL_PATH = os.path.join(__path__[0], "adsorbates.pkl")
