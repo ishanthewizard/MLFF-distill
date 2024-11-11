b0c6525fd082d0bddccd12ba076fed9d3fc4a207
Sanjeev Raja <sanjeevr@germain.lbl.gov>
2024-11-11 13:31:48 -0800

    get jmp simulation to work, but need to re-finetune to get energies

--- simulate.py
+++ simulate.py
@@ -1,5 +1,9 @@
 import os
 import torch
+import logging
+logging.basicConfig(level=logging.ERROR)
+import numpy as np
+from pathlib import Path
 from fairchem.core import OCPCalculator
 from fairchem.core.datasets import LmdbDataset
 from ase.md.langevin import Langevin
@@ -14,11 +18,51 @@ from fairchem.core.common.utils import (
     new_trainer_context,
     save_experiment_log,
     setup_logging,
+    pyg2_data_transform,
 )
 
 from fairchem.core.common.flags import flags
 
 from mace.calculators import mace_off, mace_mp
+
+# JMP stuff
+from jmp.lightning.data.balanced_batch_sampler import BalancedBatchSampler
+from torch.utils.data import DataLoader, Subset
+from torch_geometric.data import Batch, Data
+from jmp.configs.pretrain.jmp_l import jmp_l_pt_config_
+from jmp.datasets.pretrain_lmdb import PretrainLmdbDataset, PretrainDatasetConfig
+from jmp.tasks.pretrain import PretrainConfig, PretrainModel
+from jmp.tasks.pretrain.module import (
+    NormalizationConfig,
+    PretrainDatasetConfig,
+    TaskConfig,
+)
+from jmp.configs.finetune.jmp_l import jmp_l_ft_config_
+from jmp.configs.finetune.jmp_s import jmp_s_ft_config_
+from jmp.configs.finetune.md22 import jmp_l_md22_config_
+from jmp.tasks.finetune.base import FinetuneConfigBase, FinetuneModelBase
+from jmp.tasks.finetune.md22 import MD22Config, MD22Model
+
+
+def atoms_to_batch(atoms, collate_fn):
+    # convert list of atoms to a torch.geometric.data.Batch object
+    data = []
+    for atom in atoms:
+        atomic_numbers = torch.Tensor(atom.get_atomic_numbers()).to(torch.long)
+        positions = torch.Tensor(atom.get_positions())
+        cell = torch.Tensor(np.array(atom.get_cell())).view(1, 3, 3)
+        natoms = positions.shape[0]
+
+        data.append(
+            Data(
+                cell=cell,
+                pos=positions,
+                atomic_numbers=atomic_numbers,
+                natoms=natoms,
+                tags = 2*torch.ones(natoms)
+            )
+        )
+    return collate_fn(data)
 
 
 if __name__ == "__main__":
@@ -27,7 +71,6 @@ if __name__ == "__main__":
     setup_logging()
 
     parser = flags.get_parser()
-    parser.add_argument("--nersc", action="store_true", help="Run with NERSC")
     args, override_args = parser.parse_known_args()
     config = build_config(args, override_args)
     if args.timestamp_id is not None and len(args.identifier) == 0:
@@ -53,25 +96,96 @@ if __name__ == "__main__":
 
     atoms.calc = calc
 
+    if "jmp" in config.keys():
+        jmp = True
+        jmp_args = config["jmp"]
+
+        jmp_config = MD22Config.draft()
+        
+        if "-l" in jmp_args["checkpoint_path"]:
+            jmp_l_ft_config_(jmp_config, jmp_args["checkpoint_path"], disable_force_output_heads = not jmp_args["direct_forces"])
+        else:
+            jmp_s_ft_config_(jmp_config, jmp_args["checkpoint_path"], disable_force_output_heads = not jmp_args["direct_forces"])
+        
+        # This loads the MD22-specific configuration
+        jmp_l_md22_config_(jmp_config, jmp_args["molecule"], Path(jmp_args["data_path"]), direct_forces=jmp_args["direct_forces"])
+        jmp_config = jmp_config.finalize()  # Actually construct the config object
+        model = MD22Model(jmp_config)
+
+        ckpt_path = Path(jmp_args["checkpoint_path"])
+        
+        # Load the checkpoint
+        state_dict = torch.load(ckpt_path)
+        sd = state_dict["state_dict"].copy()
+        new_sd = {}
+        # Map key to the right spot since only doing trans1x for now
+        for k in sd:
+            # TODO: make sure that this is the transition 1x head
+            if "output.out_energy.3" in k:
+                new_k = k.replace("output.out_energy.3", "output.out_energy.0")
+                new_sd[new_k] = sd[k]
+            elif "output.out_forces.3" in k:
+                new_k = k.replace("output.out_forces.3", "output.out_forces.0")
+                new_sd[new_k] = sd[k]
+            elif "output.out_forces" in k or "output.out_energy" in k or "task_steps" in k:
+                pass
+            else:
+                new_sd[k] = sd[k]
+        
+        model.load_state_dict(new_sd, strict=True)
+        model.to("cuda")
+
+        # get normalization constants
+        mean_force = jmp_config.normalization['force'].mean
+        std_force = jmp_config.normalization['force'].std
+
+        mean_energy = jmp_config.normalization['y'].mean
+        std_energy = jmp_config.normalization['y'].std
+
+        
+        def get_forces(atoms):
+            
+            batch = atoms_to_batch([atoms], collate_fn=model.collate_fn)
+            batch = batch.to("cuda")
+            with torch.no_grad():
+                forces = model(batch)['force']
+            # Normalize the forces
+            forces = forces * std_force + mean_force
+            return forces.detach().cpu().numpy()
+        
+        def get_potential_energy(atoms):
+            batch = atoms_to_batch([atoms], collate_fn=model.collate_fn)
+            batch = batch.to("cuda")
+            
+            energy = model(batch)['energy']
+            
+            # Normalize the forces
+            energy = energy * std_energy + mean_energy
+            return forces
+
+        # override the calculator functions with the custom JMP ones
+        # calc.get_potential_energy = get_potential_energy # TODO: need to re-finetune JMP with energies :(
+        calc.get_forces = get_forces
+
     # Initialize atom velocities
     MaxwellBoltzmannDistribution(atoms, temperature_K=config["integrator_config"]["temperature"])
     Stationary(atoms)  # zero the center of mass velocity
 
-
+    traj_path = os.path.dirname(checkpoint_path) if not jmp else os.path.dirname(jmp_args["checkpoint_path"])
     if config["nvt"]:
         dyn = Langevin(
             atoms,
             timestep=config["integrator_config"]["timestep"] * units.fs,
             temperature_K=config["integrator_config"]["temperature"],
             friction=config["integrator_config"]["friction"] / units.fs,
-            # trajectory=os.path.join(os.path.dirname(checkpoint_path), f"equilibration{idx}.traj"),
+            trajectory=os.path.join(traj_path, f"equilibration{idx}.traj"),
         )
     else:
         """Velocity Verlet is becoming unstable for Solvated Amino Acids for some reason"""
         dyn = VelocityVerlet(
             atoms,
             timestep=config["integrator_config"]["timestep"] * units.fs,
-            trajectory=os.path.join(os.path.dirname(checkpoint_path), f"md_system{idx}.traj"),
+            trajectory=os.path.join(traj_path, f"md_system{idx}.traj"),
         )
     
 
