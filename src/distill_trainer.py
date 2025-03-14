fix another bug with sampling wrong rows from teacher hessian

src/distill_trainer.py
@@ -205,7 +205,7 @@ class DistillTrainer(OCPTrainer):
         with torch.no_grad():
             if self.config["optim"].get("active_learning", False):
                 if (
-                    int(self.epoch - 0.1)
+                    int(self.epoch-0.1)
                     % self.config["optim"].get("worst_force_update_freq", 5)
                     == 0
                 ):
@@ -224,14 +224,10 @@ class DistillTrainer(OCPTrainer):
             loss.append(en_dist_coeff * energy_jac_loss)
 
         if self.config["optim"].get("active_learning", False):
-            try:
-                worst_force_rows = [
-                    self.worst_force_row_dict[fid.item()] for fid in batch.fid
-                ]
-            except KeyError:
-                import pdb
-
-                pdb.set_trace()
+            worst_force_rows = [
+                self.worst_force_row_dict[fid.item()] for fid in batch.fid
+            ]
+            
             worst_force_rows = torch.cat(
                 [
                     rows + offset

src/distill_utils.py
@@ -198,41 +198,40 @@ def get_force_jac_loss(
     num_free_atoms_per_mol = torch.tensor(
         [sum(sub_mask) for sub_mask in mask_per_mol], device=natoms.device
     )
-    cum_jac_indexes = [0] + torch.cumsum(
-        (num_free_atoms_per_mol * natoms) * 9, dim=0
-    ).tolist()
+    cum_jac_indexes = [0] + torch.cumsum(natoms**2 * 9, dim=0).tolist()
 
     jacs_per_mol = [
         jac[: len(mol_samps), cum_sum : cum_sum + nat, :]
         for mol_samps, cum_sum, nat in zip(by_molecule, cumulative_sums[:-1], natoms)
     ]
-    if not active_learning:
-        jacs_per_mol = [
-            mol_jac[:, mask, :] for mol_jac, mask in zip(jacs_per_mol, mask_per_mol)
-        ]  # do the same for the student hessians
+    
+    # if not active_learning:
+    # jacs_per_mol = [
+    #     mol_jac[:, mask, :] for mol_jac, mask in zip(jacs_per_mol, mask_per_mol)
+    # ]  # do the same for the student hessians
 
     if torch.any(torch.isnan(jac)):
         raise Exception("FORCE JAC IS NAN")
 
     true_jacs_per_mol = []
     for i, samples in enumerate(by_molecule):
-        fixed_atoms = batch.fixed[cumulative_sums[i] : cumulative_sums[i + 1]]
+        fixed_atoms = ~mask[cumulative_sums[i] : cumulative_sums[i + 1]]
         fixed_cumsum = torch.cumsum(fixed_atoms, dim=0)
-        num_free_atoms = num_free_atoms_per_mol[i]
-
         curr = batch["force_jacs"][cum_jac_indexes[i] : cum_jac_indexes[i + 1]].reshape(
-            num_free_atoms, 3, natoms[i], 3
+            natoms[i], 3, natoms[i], 3
         )
 
-        if not active_learning:
-            curr = curr[:, :, mask_per_mol[i], :]  # filter out the masked columns
-            subsampled_curr = curr[
-                (samples[:, 0] - fixed_cumsum[samples[:, 0]]).long(), samples[:, 1]
-            ]  # get the sampled rows
-        else:
-            valid_rows = torch.where(mask_per_mol[i])[0]
-            keep = torch.where(samples[:, 0] == valid_rows)[0]
-            subsampled_curr = curr[keep.long(), samples[:, 1]]  # get the sampled rows
+
+        # if not active_learning:
+        # curr = curr[:, :, mask_per_mol[i], :]  # filter out the masked columns
+        subsampled_curr = curr[
+            (samples[:, 0] - fixed_cumsum[samples[:, 0]]).long(), samples[:, 1]
+        ]  # get the sampled rows
+        # else:
+        #     import pdb; pdb.set_trace()
+        #     valid_rows = torch.where(mask_per_mol[i])[0]
+        #     keep = torch.where(samples[:, 0] == valid_rows)[0]
+        #     subsampled_curr = curr[keep.long(), samples[:, 1]]  # get the sampled rows
 
         true_jacs_per_mol.append(subsampled_curr)
 