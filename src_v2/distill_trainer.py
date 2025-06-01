bee29f229b5e2ecbe2115f7240e37a40c38e3941
Yue Jian <yuejian@escher.lbl.gov>
2025-05-31 17:08:40 -0700

    2

--- src_v2/distill_trainer.py
+++ src_v2/distill_trainer.py
@@ -26,6 +26,7 @@ from torchtnt.framework.fit import fit
 from fairchem.core.common import distutils
 from fairchem.core.common.utils import get_subdirectories_sorted_by_time
 from fairchem.core.components.runner import Runner
+from fairchem.core.components.train.train_runner import TrainEvalRunner
 from fairchem.core.units.mlip_unit.mlip_unit import (
     convert_train_checkpoint_to_inference_checkpoint,
 )
@@ -45,15 +46,30 @@ class distill_trainer(TrainEvalRunner):
         train_dataloader: torch.utils.data.dataloader,
         eval_dataloader: torch.utils.data.dataloader,
         train_eval_unit: Union[TrainUnit, EvalUnit, Stateful],
-        label_folder: str = None,
+        callbacks: list[Callback] | None = None,
+        max_epochs: int | None = 1,
+        evaluate_every_n_steps: Optional[int] = None,
+        max_steps: int | None = None,
+        save_inference_ckpt: bool = True,
     ):  
+        super.__init__(
+                    train_dataloader=train_dataloader,
+                    eval_dataloader=eval_dataloader,
+                    train_eval_unit=train_eval_unit,
+                    callbacks=callbacks,
+                    max_epochs=max_epochs,
+                    evaluate_every_n_steps=evaluate_every_n_steps,
+                    max_steps=max_steps,
+                    save_inference_ckpt=save_inference_ckpt,
+        )
+        
         
         self.train_dataloader = train_dataloader
         self.eval_dataloader = eval_dataloader
         self.train_eval_unit = train_eval_unit
         self.device = self.train_eval_unit.model.device
         
-        self.label_folder = label_folder
+        # self.label_folder = label_folder
         print(next(iter(self.train_dataloader)))
         # make a snapshot of the sampled indices of current rank, will dump it to a file
         self.indices_list_of_current_rank = list(self.train_dataloader.batch_sampler)
