9a1aff424b3a8f0627e663f4391cc6ea9217c61e
Ishan Amin <ishan-amin@germain.lbl.gov>
2025-06-05 12:33:44 -0700

    Finished hessian code but it is not currently yielding improvements

--- src_v2/model_wrapper.py
+++ src_v2/model_wrapper.py
@@ -16,20 +16,22 @@ class HessianModelWrapper(HydraModel):
         num_samples = data.num_samples[0]
         
         cumulative_sums = torch.cat([torch.tensor([0], device=natoms.device), torch.cumsum(natoms, 0)]) # 0... sum(natoms)
-        grad_outputs = torch.zeros((num_samples, total_num_atoms, 3)).to(forces.device)
+        grad_outputs = torch.zeros((num_samples, total_num_atoms, 3)).to(forces.device) # (num_samples, total_num_atoms, 3)
         offset_samples = data.samples.clone()
         offset_samples[:, 0] = cumulative_sums[:-1].repeat_interleave(data.num_samples)
-        grad_outputs[:, offset_samples] = 1
+        grad_outputs[:, offset_samples[:, 0], offset_samples[:, 1]] = 1
         
-        jac = get_jacobian(forces, data.pos, grad_outputs, create_graph=True, looped=looped)
-        jacs_per_mol = [jac[:, cum_sum:cum_sum + nat, :].flatten() for cum_sum, nat in zip(cumulative_sums[:-1], natoms)]
+        jac = get_jacobian(forces, data.pos, grad_outputs, create_graph=True, looped=looped) # num_samples, num_atoms, 
+        jacs_per_mol = [jac[:, cum_sum:cum_sum + nat, :] for cum_sum, nat in zip(cumulative_sums[:-1], natoms)] # arr where each elem is (num_samples, num_atoms, 3)
+        jacs_per_mol = [jac.permute(1, 0, 2).reshape(nat, -1) for jac, nat in zip(jacs_per_mol, natoms)] # (arr where each elem is (num_atoms, num_samples *3))
 
         return torch.cat(jacs_per_mol)
         
     def forward(self, data):
         data.pos = data.pos.detach().requires_grad_(True)
         out = super().forward(data)
-        force_jacs = self.get_sampled_hessian(data, out)
-        out['force_jacs'] = force_jacs
+        is_validating = torch.all(data['forces_jac'] == 0)
+        force_jacs = torch.zeros((sum(data.natoms), data.num_samples[0] * 3), device=data.pos.device)  if is_validating else self.get_sampled_hessian(data, out) # torch.zeros((sum(data.natoms), data.num_samples[0] * 3))
+        out['forces_jac'] = {'forces_jac': force_jacs}
         return out
         
