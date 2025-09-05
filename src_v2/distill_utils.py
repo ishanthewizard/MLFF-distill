569efd7564588379a424ae7ef54527b6d128da94
Ishan Amin <iamin@gh-login02.delta.ncsa.illinois.edu>
2025-09-05 15:52:13 -0500

    Sme changeds

--- src_v2/distill_utils.py
+++ src_v2/distill_utils.py
@@ -187,8 +187,8 @@ def get_jacobian_finite_difference(forces, batch, grad_outputs, forward, detach,
         perturbed_forces = forward(large_batch)[force_keyword]['forces']
     else:
         perturbed_forces = []
-        for batch in tqdm(perturbed_batches):
-            pert_force = forward(batch)[force_keyword]['forces'].detach() if detach else forward(batch)[force_keyword]['forces']
+        for p_batch in perturbed_batches:
+            pert_force = forward(p_batch)[force_keyword]['forces'].detach() if detach else forward(p_batch)[force_keyword]['forces']
             perturbed_forces.append(pert_force)
         perturbed_forces = torch.cat(perturbed_forces, dim=0)
     # Split the large batch's forces into individual forward and backward forces
